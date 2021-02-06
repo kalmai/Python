d278d6af4afb8ff60f6d6b4adc2a9687392bd75b
kalmai <leozverres@gmail.com>
2021-02-06 14:08:26 -0500

    first commit

---
+++ hello_world.py
@@ -0,0 +1,21 @@
+# ye ol classic
+print("hello world")
+x = 1
+if x == 1:
+    print("x = 1")
+
+# numbers
+my_num = 7.0
+print(my_num)
+my_num = float(7)
+print(my_num)
+
+# string
+str = "this is an example string"
+print(str)
+
+# assign multiple variables on one line
+a, b = "yaas", "queen"
+print(a,b)
+a = "nooo"
+print(a,b)
